6d4f2f0b52080d34cb272b040cd76f0dca658df3
Sabrina hellen <103941912+Sabrinahellen@users.noreply.github.com>
2025-04-04 11:14:33 -0300

    feat: adicionando divisão

--- atividade.java
+++ atividade.java
@@ -1,37 +1,51 @@
 public class Main {
 
-private int numero1;
-private int numero2;
+    private int numero1;
+    private int numero2;
 
-public Main(int numero1, int numero2) {
-this.numero1 = numero1;
-this.numero2 = numero2;
-}
+    public Main(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
 
-public int somar() {
-    return this.numero1 + this.numero2;
-}
+    public int somar() {
+        return this.numero1 + this.numero2;
+    }
 
-public int subtrair() {
-    return this.numero1 - this.numero2;
-}
+    public int subtrair() {
+        return this.numero1 - this.numero2;
+    }
 
-public int multiplicar() {
-    return this.numero1 * this.numero2;
-}
+    public int multiplicar() {
+        return this.numero1 * this.numero2;
+    }
 
-public static void main(String[] args) {
+    public double dividir() {
+    
+        if (this.numero2 != 0) {
+            return (double) this.numero1 / this.numero2;
+        } else {
+            System.out.println("Erro: Não é possível dividir por zero.");
+            return 0; // Retorna 0 em caso de erro
+        }
+    }
 
-Main calculadora = new Main(5, 3);
+    public static void main(String[] args) {
 
-int resultadoSoma = calculadora.somar();
-System.out.println("A soma de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoSoma);
+        Main calculadora = new Main(5, 0); // Teste com divisor igual a 0 para verificar o erro
 
-int resultadoSubtracao = calculadora.subtrair();
-System.out.println("A subtração de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoSubtracao);
+        int resultadoSoma = calculadora.somar();
+        System.out.println("A soma de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoSoma);
 
-int resultadoMultiplicacao = calculadora.multiplicar();
-System.out.println("A multiplicação de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoMultiplicacao);
+        int resultadoSubtracao = calculadora.subtrair();
+        System.out.println("A subtração de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoSubtracao);
 
+        int resultadoMultiplicacao = calculadora.multiplicar();
+        System.out.println("A multiplicação de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoMultiplicacao);
+
+        double resultadoDivisao = calculadora.dividir();
+        if (resultadoDivisao != 0) {
+            System.out.println("A divisão de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoDivisao);
+        }
     }
 }

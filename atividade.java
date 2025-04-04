e905eb48ecd1d686e945b507c46131c568b1989a
sarahat4am <117488834+sarahat4am@users.noreply.github.com>
2025-04-04 11:07:08 -0300

    feat: adiciona função

--- atividade.java
+++ atividade.java
@@ -3,20 +3,22 @@ public class Main {
 private int numero1;
 private int numero2;
 
-
 public Main(int numero1, int numero2) {
 this.numero1 = numero1;
 this.numero2 = numero2;
 }
 
-
 public int somar() {
-return this.numero1 + this.numero2;
+    return this.numero1 + this.numero2;
 }
 
- public int subtrair() {
-        return this.numero1 - this.numero2;
-    }
+public int subtrair() {
+    return this.numero1 - this.numero2;
+}
+
+public int multiplicar() {
+    return this.numero1 * this.numero2;
+}
 
 public static void main(String[] args) {
 
@@ -27,6 +29,9 @@ System.out.println("A soma de " + calculadora.numero1 + " e " + calculadora.nume
 
 int resultadoSubtracao = calculadora.subtrair();
 System.out.println("A subtração de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoSubtracao);
+
+int resultadoMultiplicacao = calculadora.multiplicar();
+System.out.println("A multiplicação de " + calculadora.numero1 + " e " + calculadora.numero2 + " é: " + resultadoMultiplicacao);
 
     }
 }
